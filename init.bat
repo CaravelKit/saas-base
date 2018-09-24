1df25e44fde5e9b4a025511f37c2175c2c08ae2d
SaaS-Idea <info@saas-idea.com>
2018-09-23 22:51:18 -0600

    Fixed fonts reference

--- init.bat
+++ init.bat
@@ -1,4 +1,7 @@
-npm install
-flask dbinit -c
-npm run dev
-flask run
+call venv\Scripts\activate.bat
+call python -m pip install --upgrade pip
+call pip install -r requirements.txt
+call npm install
+call flask dbinit -c
+call npm run dev
+call flask run
