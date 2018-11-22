f6db31066c09fc21a7ffa4774285acd947ce5739
SaaS Idea Admin <info@saas-idea.com>
2018-11-21 18:57:37 -0700

    First commit to our dashboard

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = ''
     CSRF_KEY = ''
-    MAIL_SUBJECT_PREFIX = 'Your service name'
+    MAIL_SUBJECT_PREFIX = 'SaaS Idea'
     COMPANY_NAME = 'Your company name' # Change to your company name
     TRIAL_PERIOD_IN_DAYS = 14 # Change to your trial (in days, it's 2 weeks by default). Put 0 if no trial
     SAAS_API_KEY = get_secure_variable('saas_api_key') # Add after you create an account at www.saasidea.io
