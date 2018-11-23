36312a59b770ed8390b16453313ebcfc0ace2bd2
SaaS-Idea <info@saas-idea.com>
2018-11-23 13:59:25 -0700

    Change database shell commands

--- application.py
+++ application.py
@@ -18,12 +18,16 @@ def dbinit(dboption):
 
 @application.cli.command()
 @with_appcontext
-@click.argument('interface_element')
-@click.option('-u', 'option', flag_value='update',
-              default='')
-def scaffold(interface_element, option):
-    from scaffold.generators.interface import generate
-    generate(interface_element, option)
+def dbupdate():
+    print('update db')
+    init_db('update', application)
+
+@application.cli.command()
+@with_appcontext
+def dbcreate():
+    print('create db')
+    init_db('create', application)
+
 
 # to-do:
 # To make it work with application.py I need to change set FLASK_APP=main to set FLASK_APP=application in venv
