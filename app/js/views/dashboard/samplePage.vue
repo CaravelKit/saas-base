fed5ea201d7ead0e53f371460d51388a90132927
SaaS Idea Admin <info@saas-idea.com>
2018-12-16 12:45:23 -0700

    Fixed styles

--- app/js/views/dashboard/samplePage.vue
+++ app/js/views/dashboard/samplePage.vue
@@ -3,11 +3,50 @@
         <div class="card mb-3">
             <div class="card-header">
             <i class="fas fa-table"></i>
-            Data Table Example</div>
-            <div class="card-body">
-            <div class="table-responsive">
-            </div>
-            </div>
+            Card with table inside</div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">First name</th>
+                                    <th scope="col">Last name</th>
+                                    <th scope="col">Position</th>
+                                    <th scope="col">What to do with him/her</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Michael</td>
+                                    <td>Bolton</td>
+                                    <td>Programmer</td>
+                                    <td>
+                                        <button class="btn btn-danger">Fire</button>
+                                        <button class="btn btn-success">Promote</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Samir</td>
+                                    <td>Nagheenanajar</td>
+                                    <td>Programmer</td>
+                                    <td>
+                                        <button class="btn btn-danger">Fire</button>
+                                        <button class="btn btn-success">Promote</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Bill</td>
+                                    <td>Lumbergh</td>
+                                    <td>Vice president</td>
+                                    <td>
+                                        <button class="btn btn-danger">Fire</button>
+                                        <button class="btn btn-success">Promote</button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
         
         </div> THIS IS VERY LONG PAGE with a sample table
